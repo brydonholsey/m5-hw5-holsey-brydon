19ca96fcc5262102eb316a66b1a211a3f106a6f3
Brydon Holsey <brydon.holsey@ufl.edu>
2020-02-09 20:44:02 -0500

    Add objects to pizza variable

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+console.log('hello world!');
+
 var pizza = {
-    crust: thin,
-    cheese: mozzarella,
-    sauce: red,
-    addToppings: function() {
-        console.log('Please add pepperoni and sausage')
+    crust: 'thin',
+    cheese: 'mozzarella',
+    sauce: 'red',
+    addToppings: function () {
+        console.log('Please add pepperoni and sausage');
     }
-}
+};
+
+function orderPizza() {
+    console.log(pizza[0]);
+};
